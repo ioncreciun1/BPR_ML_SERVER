ac97fbec7375918caf8782bf0b7f308341253427
ion-chromaviso <ion.creciun1@gmail.com>
2022-12-12 14:49:17 +0100

    update server.py by adding CORS

--- App/server.py
+++ App/server.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask import request
 import json
 from signLanguageModel import SignLanguageModel
+from flask_cors import CORS,cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
 
+
+@cross_origin()
 @app.route("/sign-language-translation", methods = ["POST"])
 def get_word():
   data = json.loads(request.data)
@@ -15,4 +20,4 @@ def get_word():
 
 if __name__ == "__main__":
   model = SignLanguageModel()
-  app.run()
+  app.run( port=80)
